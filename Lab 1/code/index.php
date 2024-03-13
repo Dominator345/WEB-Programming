d421e3f7514c4305cf91af1b2aaabc5b2d6fe90f
Dominator345 <Gusenichnayagusenica27@gmail.com>
2024-03-13 23:11:19 +0200

    Task 1

--- Lab 1/code/index.php
+++ Lab 1/code/index.php
@@ -1,2 +1,19 @@
 <?php
-echo phpinfo();
+
+
+// Task 1 "Доступ по ссылке"
+
+echo "Task 1 <br /> <br />";
+
+/* Imagine a lot of code here */ 
+ $very_bad_unclear_name = "15 chicken wings"; // А в лекции Вы говорили что так не стоит обозначать переменные;)
+
+
+// Write your code here:
+$order = &$very_bad_unclear_name;
+$order .= ' queue';
+
+
+ // Don't change the line below
+ echo "\nYour order is: $very_bad_unclear_name.";
+ echo "<br /> <br />";
