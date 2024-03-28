25fba2cada8a76a43d4277ae12c00a5c92e9f265
Dominator345 <Gusenichnayagusenica27@gmail.com>
2024-03-28 19:07:12 +0200

    Reworked output in Task 1 (a) and (b)

--- Lab 3/code/index.php
+++ Lab 3/code/index.php
@@ -2,10 +2,13 @@
 
 // Part a.
 
+echo 'Task 1 (a)' . '<br />';
+
 $str = 'ahb acb aeb aeeb adcb axeb';
 $wantedString = '/a..b/u';
 $matches = [];
 preg_match_all($wantedString, $str, $matches);
+echo 'Result: ' . '<br />';
 foreach($matches[0] as $match)
 {
     echo $match . '<br />';
@@ -15,6 +18,8 @@ echo '<br />';
 
 // Part b.
 
+echo 'Task 2 (b)' . '<br />';
+
 function replaceCube($match) : string
 {
     return $match[0] ** 3;
@@ -23,4 +28,4 @@ function replaceCube($match) : string
 $str = 'a1b2c3';
 $wantedString = '/[0-9]/';
 $result = preg_replace_callback($wantedString, 'replaceCube', $str);
-echo $result;
+echo 'Result: ' . '<br />' . $result;
