d1ca38fe44d47f7c485641143ec84051443d87e1
Dominator345 <Gusenichnayagusenica27@gmail.com>
2024-03-28 18:59:27 +0200

    Task 1(b)

--- Lab 3/code/index.php
+++ Lab 3/code/index.php
@@ -11,3 +11,16 @@ foreach($matches[0] as $match)
     echo $match . '<br />';
 }
 echo '<br />';
+
+
+// Part b.
+
+function replaceCube($match) : string
+{
+    return $match[0] ** 3;
+}
+
+$str = 'a1b2c3';
+$wantedString = '/[0-9]/';
+$result = preg_replace_callback($wantedString, 'replaceCube', $str);
+echo $result;
