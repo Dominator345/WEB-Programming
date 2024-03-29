8d5ea77bb7b09678988d364221b862d315e2b99a
Dominator345 <Gusenichnayagusenica27@gmail.com>
2024-03-29 12:34:34 +0200

    Create form for Task 2(c)

---
+++ Lab 3/code/Task2c.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task 2 Part a</title>
+</head>
+<body>
+    <form action="/Task2cResult.php" method="post">
+        <label for="name">Имя</label>
+        <input type="text" name="name" required >
+        <br><br>
+        <label for="age">Возраст</label>
+        <input type="text" name="age" required>
+        <br><br>
+        <label for="salary">Зарплата</label>
+        <input type="text" name="salary" required>
+        <br><br>
+        <label for="city">Город</label>
+        <input type="text" name="city" required>
+        <br><br>
+        <input type="submit" value="Save">
+    </form>
+</body>
+</html>
