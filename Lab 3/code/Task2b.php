7e950f1ceddd8f51a36b04a1f0f43bcdacaf30fb
Dominator345 <Gusenichnayagusenica27@gmail.com>
2024-03-29 12:33:39 +0200

    Rework Task 2(b)
    Added data saving to the session

--- Lab 3/code/Task2b.php
+++ Lab 3/code/Task2b.php
@@ -7,13 +7,13 @@
     <form action="/Task2bResult.php" method="post">
         <label for="surname">Фамилия</label>
         <input type="text" name="surname" required >
-        <br>
+        <br><br>
         <label for="name">Имя</label>
         <input type="text" name="name" required>
-        <br>
+        <br><br>
         <label for="age">Возраст</label>
         <input type="text" name="age" required>
-        <br>
+        <br><br>
         <input type="submit" value="Save">
     </form>
 </body>
