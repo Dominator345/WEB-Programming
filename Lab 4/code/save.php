6a4546e8f18bda237707d31d65340637d9b30f22
Dominator345 <Gusenichnayagusenica27@gmail.com>
2024-05-03 14:13:15 +0200

    Made data records in a table

--- Lab 4/code/save.php
+++ Lab 4/code/save.php
@@ -9,6 +9,23 @@ $description = $_POST['description'];
 $title = $_POST['title'];
 $email = $_POST['email'];
 
+$client = new Google_Client();
+$client->setApplicationName("LabaFour");
+$client->setAuthConfig('googleAPI.json');
+$client->addScope(Google_Service_Sheets::SPREADSHEETS);
+$client->setAccessType('offline');
+
+$service = new Google_Service_Sheets($client);
+
+$spreadsheetId = "1_gX7ZgUKcoog_dYvWZ4o6XdeaUtWqdlhI_FMYhk_Cp4";
+
+$values = [[$category, $email, $title, $description]];
+
+$range = "A1:D20";
+$body = new Google_Service_Sheets_ValueRange(["values" => $values]);
+$param = ["valueInputOption" => "RAW"];
+
+$result = $service->spreadsheets_values->append($spreadsheetId, $range, $body, $param);
 
 
 redirectToHome();
